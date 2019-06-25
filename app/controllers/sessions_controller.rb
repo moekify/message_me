af1fdce3965075b2e11cf8596af9de80e65cb0e8
moekify <mookie@DESKTOP-PMSJUD2.localdomain>
2019-06-25 22:38:48 +0200

    Controller level restrictions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
-  def new
+  before_action :logged_in_redirect, only: [:new, :create]
 
+
+  def new
   end
 
   def create
@@ -22,4 +24,14 @@ class SessionsController < ApplicationController
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
   end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
+  end
+
 end
