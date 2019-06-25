d7231fc4a712c3f1dad66868e5f8eedee6bd30e4
moekify <mookie@DESKTOP-PMSJUD2.localdomain>
2019-06-25 22:23:49 +0200

    Sessions Controller and login/logout routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
   def new
+
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      # good scenario
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else 
+      flash.now[:error] = "There was something wrong with your login information"
+      render 'new'
+      # invalid user or invalid password
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
   end
 end
